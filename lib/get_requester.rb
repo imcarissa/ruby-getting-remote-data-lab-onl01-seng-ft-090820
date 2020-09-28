95f7c3e7f476d085031b1366f08d5ce1fa0bd3df
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-28 11:02:51 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -4,21 +4,19 @@ require 'json'
 
 
 class GetRequester
-  
-    attr_accessor :url 
     
     def initialize(url)
       @url = url 
     end 
     
     def get_response_body
-      uri = URL.parse(url)
+      uri = URI.parse(@url)
       response = Net::HTTP.get_response(uri)
       response.body
     end
     
     def parse_json
-      JSON.parse.(self.get_response_body)
+      JSON.parse(self.get_response_body)
     end
     
 end
