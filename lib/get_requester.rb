1ba631932134b3e1dbeea6f9c21f28f36c8eccc3
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-28 10:56:54 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -9,12 +9,12 @@ class GetRequester
     
     def initialize(url)
       @url = url 
-      @url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
     end 
     
     def get_response_body
       uri = URL.parse(url)
-      uri.open.string 
+      response = Net::HTTP.get_response(uri)
+      response.body
     end
     
     def parse_json
